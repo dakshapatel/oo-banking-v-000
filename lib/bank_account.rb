b8baa1d95fc38512f1731272c7cb30d456daeb9f
Daksha Patel <dakshapatel100@gmail.com>
2018-05-15 04:38:40 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -4,10 +4,10 @@ class BankAccount
 attr_reader :name
 attr_accessor :balance, :status
 
-def initialize(name, balance = 1000)
+def initialize(name)
   @name = name
   @status = "open"
-  @balance = balance
+  @balance = 1000
 
 end
 
